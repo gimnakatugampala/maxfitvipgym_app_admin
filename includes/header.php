d45fa03aac908f6d3d403fc597194348c439b62a
gimnakatugampala <gimnakatugampala1@gmail.com>
2025-02-19 20:06:25 +0530

    header

---
+++ includes/header.php
@@ -0,0 +1,29 @@
+<link href="../css/bootstrap.min.css" rel="stylesheet">
+<link href="../font-awesome/css/font-awesome.css" rel="stylesheet">
+
+<link href="../css/plugins/summernote/summernote-bs4.css" rel="stylesheet">
+
+<link href="../css/plugins/iCheck/custom.css" rel="stylesheet">
+
+<link href="../css/plugins/dropzone/basic.css" rel="stylesheet">
+<link href="../css/plugins/dropzone/dropzone.css" rel="stylesheet">
+<link href="../css/plugins/jasny/jasny-bootstrap.min.css" rel="stylesheet">
+<link href="../css/plugins/datapicker/datepicker3.css" rel="stylesheet">
+
+<!-- FooTable -->
+<link href="../css/plugins/footable/footable.core.css" rel="stylesheet">
+
+<!-- Ladda style -->
+<link href="../css/plugins/ladda/ladda-themeless.min.css" rel="stylesheet">
+
+<link href="../css/animate.css" rel="stylesheet">
+<link href="../css/style.css" rel="stylesheet">
+
+<link href="../css/plugins/awesome-bootstrap-checkbox/awesome-bootstrap-checkbox.css" rel="stylesheet">
+
+
+<link href="../css/plugins/dataTables/datatables.min.css" rel="stylesheet">
+
+
+<link href="../css/plugins/slick/slick.css" rel="stylesheet">
+<link href="../css/plugins/slick/slick-theme.css" rel="stylesheet">

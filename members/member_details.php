9e71938955d0ad97487dc57411e921829baad9a1
gimnakatugampala <gimnakatugampala1@gmail.com>
2025-02-20 19:32:14 +0530

    member

--- members/member_details.php
+++ members/member_details.php
@@ -10,6 +10,26 @@
 
     <?php include '../includes/header.php';  ?>
 
+    <style>
+    
+    .profile-info h1 {
+            font-size: 24px;
+            font-weight: bold;
+        }
+        .profile-info h5 {
+            font-size: 16px;
+            color: #555;
+        }
+        .details {
+            margin-top: 15px;
+        }
+        .details h5 {
+            font-size: 14px;
+            color: #6c757d;
+            margin-bottom: 5px;
+        }
+    </style>
+
 </head>
 
 <body>
@@ -44,71 +64,105 @@
 
         <div class="wrapper wrapper-content animated fadeInRight">
 
-            <div class="row">
-                <div class="col-lg-12">
+        <div class="tabs-container">
+                        <ul class="nav nav-tabs" role="tablist">
+                            <li><a class="nav-link active" data-toggle="tab" href="#tab-1"> Member Info</a></li>
+                            <li><a class="nav-link" data-toggle="tab" href="#tab-2">Workout Schedule</a></li>
+                            <li><a class="nav-link" data-toggle="tab" href="#tab-3">Body Stats</a></li>
+                            <li><a class="nav-link" data-toggle="tab" href="#tab-4">Attendance</a></li>
+                        </ul>
+                        <div class="tab-content">
 
-                    <div class="ibox product-detail">
-                        <div class="ibox-content">
+                            <div role="tabpanel" id="tab-1" class="tab-pane active">
+                                <div class="panel-body">
+                                <div class="row">
+                                <div class="col-lg-12">
+                                    <div class="ibox product-detail">
+                                        <div class="ibox-content">
 
-                            <div class="row">
-                                <div class="col-md-12">
-
-                                    <h2 class="font-bold m-b-xs">
-                                        Desktop publishing software
-                                    </h2>
-                                    <small>Many desktop publishing packages and web page editors now.</small>
-                                    <div class="m-t-md">
-                                        <h2 class="product-main-price">$406,602 <small class="text-muted">Exclude Tax</small> </h2>
-                                    </div>
-                                    <hr>
-
-                                    <h4>Product description</h4>
-
-                                    <div class="small text-muted">
-                                        It is a long established fact that a reader will be distracted by the readable
-                                        content of a page when looking at its layout. The point of using Lorem Ipsum is
-
-                                        <br/>
-                                        <br/>
-                                        There are many variations of passages of Lorem Ipsum available, but the majority
-                                        have suffered alteration in some form, by injected humour, or randomised words
-                                        which don't look even slightly believable.
-                                    </div>
-                                    <dl class="small m-t-md">
-                                        <dt>Description lists</dt>
-                                        <dd>A description list is perfect for defining terms.</dd>
-                                        <dt>Euismod</dt>
-                                        <dd>Vestibulum id ligula porta felis euismod semper eget lacinia odio sem nec elit.</dd>
-                                        <dd>Donec id elit non mi porta gravida at eget metus.</dd>
-                                        <dt>Malesuada porta</dt>
-                                        <dd>Etiam porta sem malesuada magna mollis euismod.</dd>
-                                    </dl>
-                                    <hr>
-
-                                    <div>
-                                        <div class="btn-group">
-                                            <button class="btn btn-primary btn-sm"><i class="fa fa-cart-plus"></i> Add to cart</button>
-                                            <button class="btn btn-white btn-sm"><i class="fa fa-star"></i> Add to wishlist </button>
-                                            <button class="btn btn-white btn-sm"><i class="fa fa-envelope"></i> Contact with author </button>
+                                        <div class="text-center">
+                                        <div class="row mx-auto">
+                                            <div class="col-md-12">
+                                                <img alt="Profile Image" class="rounded-circle img-fluid" src="../img/a7.jpg" style="width: 120px; height: 120px;">
+                                                <div class="mt-3">
+                                                    <h1 class="h2 fw-bold">GIMNA KATUGAMPALA</h1>
+                                                    <h4 class="text-muted">+94 764961707</h4>
+                                                </div>
+                                            </div>
                                         </div>
+                                        <div class="row mt-3 text-start">
+                                            <div class="col-6">
+                                                <h4 class="text-muted">Member since:</h4>
+                                                <h3 class="fw-semibold">20th February 2025</h3>
+                                            </div>
+                                            <div class="col-6">
+                                                <h4 class="text-muted">Next Payment:</h4>
+                                                <h3 class="fw-semibold">20th February 2025</h3>
+                                            </div>
+                                            <div class="col-6">
+                                                <h4 class="text-muted">Last Active:</h4>
+                                                <h3 class="fw-semibold">20th February 2025</h3>
+                                            </div>
+                                            <div class="col-6">
+                                                <h4 class="text-muted">Expiry Date:</h4>
+                                                <h3 class="fw-semibold">20th February 2025</h3>
+                                            </div>
+                                        </div>
+                                        <hr>
                                     </div>
 
-
+                                        </div>
+                                       
+                                    </div>
 
                                 </div>
                             </div>
+                                </div>
+                            </div>
+
+                            <div role="tabpanel" id="tab-2" class="tab-pane">
+                                <div class="panel-body">
+                                    <strong>Donec quam felis</strong>
+
+                                    <p>Thousand unknown plants are noticed by me: when I hear the buzz of the little world among the stalks, and grow familiar with the countless indescribable forms of the insects
+                                        and flies, then I feel the presence of the Almighty, who formed us in his own image, and the breath </p>
+
+                                    <p>I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine. I am so happy, my dear friend, so absorbed in the exquisite
+                                        sense of mere tranquil existence, that I neglect my talents. I should be incapable of drawing a single stroke at the present moment; and yet.</p>
+                                </div>
+                            </div>
+
+                            <div role="tabpanel" id="tab-3" class="tab-pane">
+                                <div class="panel-body">
+                                    <strong>Donec quam felis</strong>
+
+                                    <p>Thousand unknown plants are noticed by me: when I hear the buzz of the little world among the stalks, and grow familiar with the countless indescribable forms of the insects
+                                        and flies, then I feel the presence of the Almighty, who formed us in his own image, and the breath </p>
+
+                                    <p>I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine. I am so happy, my dear friend, so absorbed in the exquisite
+                                        sense of mere tranquil existence, that I neglect my talents. I should be incapable of drawing a single stroke at the present moment; and yet.</p>
+                                </div>
+                            </div>
+
+                            <div role="tabpanel" id="tab-4" class="tab-pane">
+                                <div class="panel-body">
+                                    <strong>Donec quam felis</strong>
+
+                                    <p>Thousand unknown plants are noticed by me: when I hear the buzz of the little world among the stalks, and grow familiar with the countless indescribable forms of the insects
+                                        and flies, then I feel the presence of the Almighty, who formed us in his own image, and the breath </p>
+
+                                    <p>I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine. I am so happy, my dear friend, so absorbed in the exquisite
+                                        sense of mere tranquil existence, that I neglect my talents. I should be incapable of drawing a single stroke at the present moment; and yet.</p>
+                                </div>
+                            </div>
+
 
                         </div>
-                        <div class="ibox-footer">
-                            <span class="float-right">
-                                Full stock - <i class="fa fa-clock-o"></i> 14.04.2016 10:04 pm
-                            </span>
-                            The generated Lorem Ipsum is therefore always free
-                        </div>
+
+
                     </div>
 
-                </div>
-            </div>
+            
             
 
 

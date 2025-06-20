42541bec459760a52054be91c655d23d6926945b
gimnakatugampala <gimnakatugampala1@gmail.com>
2025-06-20 12:12:05 +0530

    members

--- members/pending_members.php
+++ members/pending_members.php
@@ -120,69 +120,6 @@
         </div>
         </div>
 
-<!-- Modal -->
-        <div class="modal inmodal fade" id="myModal5" tabindex="-1" role="dialog"  aria-hidden="true">
-            <div class="modal-dialog modal-lg">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-                        <h4 class="modal-title">Add Body Stats</h4>
-                        <small class="font-bold">20th February 2025 11:00PM</small>
-                    </div>
-                    <div class="modal-body">
-
-                    <div class="form-group  row"><label class="col-sm-3 col-form-label"><b>Weight [kg]</b>:<span class="text-danger">*</span> </label>
-                    <div class="col-sm-9"><input type="text" class="form-control"></div>
-                    </div>
-                    <div class="hr-line-dashed"></div>
-
-                    <div class="form-group  row"><label class="col-sm-3 col-form-label"><b>Hip Size [inch]</b>:<span class="text-danger">*</span> </label>
-                    <div class="col-sm-9"><input type="text" class="form-control">
-                    </div>
-                    </div>
-                    <div class="hr-line-dashed"></div>
-
-                    <div class="form-group  row"><label class="col-sm-3 col-form-label"><b>Chest [inch]</b>:<span class="text-danger">*</span> </label>
-                    <div class="col-sm-9"><input type="text" class="form-control">
-                    </div>
-                    </div>
-                    <div class="hr-line-dashed"></div>
-
-                    <div class="form-group  row"><label class="col-sm-3 col-form-label"><b>Bicep [inch]</b>:<span class="text-danger">*</span> </label>
-                    <div class="col-sm-9"><input type="text" class="form-control">
-                    </div>
-                    </div>
-                    <div class="hr-line-dashed"></div>
-
-                    <div class="form-group  row"><label class="col-sm-3 col-form-label"><b>Max H/R [inch]</b>:<span class="text-danger">*</span> </label>
-                    <div class="col-sm-9"><input type="text" class="form-control">
-                    </div>
-                    </div>
-                    
-
-                    <div class="form-group  row"><label class="col-sm-3 col-form-label"><b>Payment Date</b>:<span class="text-danger">*</span> </label>
-                    <div class="col-sm-9"><input type="date" class="form-control">
-                    </div>
-                    </div>
-
-                    <div class="form-group  row"><label class="col-sm-3 col-form-label"><b>Payment Expiry Date</b>:<span class="text-danger">*</span> </label>
-                    <div class="col-sm-9"><input type="date" class="form-control">
-                    </div>
-                    </div>
-                    
-                   
-                    
-                    
-                    </div>
-
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-white" data-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary">Submit</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-
 
 
   <?php include '../includes/footer.php'; ?>
